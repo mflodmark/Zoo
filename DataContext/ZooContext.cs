386dd9167498dc6db01666d7caa85c0efebe7e69
Markus Flodmark <markus.flodmark@gmail.com>
2017-11-01 23:06:07 +0100

    Added some initial fluent API

--- DataContext/ZooContext.cs
+++ DataContext/ZooContext.cs
@@ -7,12 +7,7 @@ namespace Zoo.DataContext
 
     public class ZooContext : DbContext
     {
-        // Your context has been configured to use a 'ZooContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'Zoo.DataContext.ZooContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'ZooContext' 
-        // connection string in the application configuration file.
+        
         public ZooContext(): base("name=ZooContext")
         {
 
@@ -23,15 +18,17 @@ namespace Zoo.DataContext
         public virtual DbSet<Species> Species { get; set; }
         public virtual DbSet<Type> Types { get; set; }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Animal>().Property(x => x.Quantity).IsRequired();
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+            modelBuilder.Entity<Enviroment>().Property(x => x.Name).IsRequired();
+
+            modelBuilder.Entity<Species>().Property(x => x.Name).IsRequired();
+
+            modelBuilder.Entity<Type>().Property(x => x.Name).IsRequired();
+        }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+   
 }
