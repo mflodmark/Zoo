4f87ba2a605233b7766d6eaa6f4de90997e11b5f
Markus Flodmark <markus.flodmark@gmail.com>
2017-11-02 08:21:47 +0100

    AddValuesToComboBoxes

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Zoo.DataContext;
 
 namespace Zoo
 {
@@ -24,6 +25,45 @@ namespace Zoo
         {
 
             InitializeComponent();
+
+            AddValuesToComboBoxType();
+            AddValuesToComboBoxEnviroment();
+            AddValuesToComboBoxSpecies();
         }
+
+        #region AddValuesToCOmboBoxes
+
+        private void AddValuesToComboBoxSpecies()
+        {
+            var dbContext = new ZooContext();
+
+            foreach (var item in dbContext.Species)
+            {
+                SearchSpeciesBox.Items.Add(item.Name);
+            }
+        }
+
+        private void AddValuesToComboBoxEnviroment()
+        {
+            var dbContext = new ZooContext();
+
+            foreach (var item in dbContext.Enviroments)
+            {
+                SearchEnviromentBox.Items.Add(item.Name);
+            }
+        }
+
+        private void AddValuesToComboBoxType()
+        {
+            var dbContext = new ZooContext();
+
+            foreach (var item in dbContext.Types)
+            {
+                SearchTypeBox.Items.Add(item.Name);
+            }
+        }
+
+
+        #endregion
     }
 }
